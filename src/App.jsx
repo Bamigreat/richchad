081119ad5a44fff62e905bac96c97388bac500b6
bamidele olorunfemi <bamigreat97@gmail.com>
2025-06-18 16:59:24 +0100

    fourth

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import Hero from "./Components/Hero/Hero";
 import Chadter_1 from "./Components/Chadter_1/Chadter_1";
-import Chadter_2 from "./Components/Chadter_2/chadter_2";
+import Chadter_2 from "./Components/Chadter_2/Chadter_2";
 import Chadter_3 from "./Components/Chadter_3/chadter_3";
 import Chadter_4 from "./Components/Chadter_4/chadter_4";
 import Chadter_5 from "./Components/Chadter_5/chadter_5";
